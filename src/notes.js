0f26ba624205adf639f429f90aead59abdd40164
dikiwydi <dikiwahyudi268@gmail.com>
2023-05-11 08:14:25 +0700

    init

---
+++ src/notes.js
@@ -0,0 +1,10 @@
+const notes = [];
+
+module.exports = { notes };
+
+//     id: string,
+//     title: string,
+//     createdAt: string,
+//     updatedAt: string,
+//     tags: array of string,
+//     body: string,
